7a98892417329c4ca0d8b6d7c1fc84f53f087a7f
oopszaxyz <6110110301@psu.ac.th>
2020-08-24 21:10:12 +0700

    part 6.5

--- screen/ZipCodeScreen.js
+++ screen/ZipCodeScreen.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import { StatusBar, StyleSheet, FlatList, Text, View } from 'react-native';
+import { SafeAreaView, ImageBackground, StatusBar, StyleSheet, FlatList, Text, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { TouchableHighlight } from 'react-native-gesture-handler';
+import Carousel from 'react-native-snap-carousel';
+import { scrollInterpolator, animatedStyles } from './slideSetting';
 
 const availableZipItems = [
-    { place: 'Hatyai', code: '90110' },
-    { place: 'Trang', code: '92000' },
-    { place: 'Chiangmai', code: '50000' },
-    { place: 'Khonkaen', code: '40000' },
-    { place: 'Chonburi', code: '20000' },
-    { place: 'Bangkok', code: '10600' },
-    { place: 'Satoon', code: '91000' },
+    { place: 'Hatyai', code: '90110', background:require('../Image/hdy.jpg')},
+    { place: 'Trang', code: '92000', background:require('../Image/trang.jpg')},
+    { place: 'Chiangmai', code: '50000', background:require('../Image/chiengmai.jpg')},
+    { place: 'Khonkaen', code: '40000', background:require('../Image/khonkaen.jpg')},
+    { place: 'Chonburi', code: '20000', background:require('../Image/chonburi.jpg')},
+    { place: 'Bangkok', code: '10600', background:require('../Image/bangkok.jpg')},
+    { place: 'Satoon', code: '91000', background:require('../Image/satoon.jpg')},
 ]
 
-const ZipItem = ({place, code, navigation}) => (
+const ZipItem = ({place, code, background, navigation}) => (
     <TouchableHighlight onPress={() => 
         navigation.navigate('Weather', { zipCode: code})
     }>
         <View style={styles.zipItem}>
-            <Text style={styles.zipPlace}>{place}</Text>
-            <Text style={styles.zipCode}>{code}</Text>
+            <ImageBackground source={background} style={styles.zipItem}>
+                <Text style={styles.zipPlace}>{place}</Text>
+                <Text style={styles.zipCode}>{code}</Text>
+            </ImageBackground>
         </View>
     </TouchableHighlight>
 )
@@ -30,23 +34,47 @@ export default function ZipCodeScreen(){
     const navigation = useNavigation()
     return (
         <View>
-            <FlatList
-                data={availableZipItems}
-                keyExtractor={_keyExtractor}
-                renderItem={({item}) => <ZipItem {...item} navigation={navigation}/>}
-            />
-            <StatusBar style='auto'/>
+            <SafeAreaView style={styles.slideStyle}>
+            <View style={{ flex: 1, flexDirection:'row', justifyContent: 'center', }}>
+                <Carousel
+                  layout={'stack'} layoutCardOffset={`18`}
+                  data = {availableZipItems}
+                  keyExtractor = {item => item.code}
+                  renderItem = {({item}) => <ZipItem {...item} navigation={navigation}/>}
+
+                  scrollInterpolator={scrollInterpolator}
+                  slideInterpolatedStyle={animatedStyles}
+
+                  hasParallaxImages={true}
+                  useScrollView={true}
+
+                  sliderWidth={200}
+                  itemWidth={310}
+                  />
+            </View>
+          </SafeAreaView>
         </View>
  );
 }
 
 const styles = StyleSheet.create({  
+    backdrop: {
+        width: '100%',
+        height: '100%'
+    },
+
+    slideStyle: {
+        flex: 1, 
+        backgroundColor:'#000033',
+        paddingTop: 130,
+    },
+
     zipItem : {
         flex: 1,
         flexDirection: 'row',
         justifyContent: 'space-between',
-        backgroundColor: 'rgb(400,150,100)',
-        opacity:0.6,
+        //backgroundColor: 'rgb(400,150,100)',
+        opacity:0.9,
     },
 
     zipPlace:{
